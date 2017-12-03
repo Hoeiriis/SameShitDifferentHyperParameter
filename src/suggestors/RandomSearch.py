fa2867276ccdab6baba1b9d54838469573719c19
Jeppe <jeppe.h.m@hotmail.com>
2017-12-03 14:16:39 +0100

    Implemented RandomSearch

---
+++ src/suggestors/RandomSearch.py
@@ -0,0 +1,24 @@
+import numpy as np
+from .SuggestorBase import SuggestorBase
+
+
+class RandomSearch(SuggestorBase):
+
+    def calculate_suggestion(self):
+        unique = False
+
+        while not unique:
+            real, unscaled = self._random_param_sample()
+
+            unique = self.param_log.log_param(real, unscaled, 0)
+
+        return real
+
+    def _random_param_sample(self):
+        real_parameters = []
+        unscaled_parameters = np.random.random_sample(self.n_param)
+
+        for i, func in enumerate(self._rescale_functions.values()):
+            real_parameters.append(func(unscaled_parameters[i]))
+
+        return np.asarray(real_parameters), unscaled_parameters
